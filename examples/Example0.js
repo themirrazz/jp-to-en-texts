bb15d0ac9381bb6c3902df30e970de6704c2fcac
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-03-05 11:36:22 -0800

    dwfgg

--- examples/Example0.js
+++ examples/Example0.js
@@ -7,10 +7,10 @@ var example = {
         "タコ：私... 私は...",
         "カズ: 「お、私... 私は...」やまれよ！",
         "  お前の失敗から、みんなはしぬよ！お前の失敗から、お前のよ！",
-        "たこ：俺等できるな...",
+        "タコ：俺等できるな...",
         "カズ：これを直さないよ！このクソ野郎！",
-        "たこ：私、クソ野郎？！今、お前はクソ野郎、このうさい野郎！",
-        "カズ：は？！て目は何を言った？！",
+        "タコ：私、クソ野郎？！今、お前はクソ野郎、このうさい野郎！",
+        "カズ：は？！手名は何を言った？！",
         "タコ：えーと...何も、さよならね！",
         "カズ：その野郎..."
     ],
@@ -21,13 +21,218 @@ var example = {
                 "Kazu: "
             ],
             [
-                "You dumbass! ",
-                "You dumbshit! ",
-                "You idiot! "
+                "You dumbass!",
+                "You dumbshit!",
+                "You idiot!"
+            ],
+            [
+                " "
             ],
             [
                 "How could you?!",
                 "How could you do that?!"
+            ]
+        ],
+        [
+            [
+                "Tako: "
+            ],
+            [
+                "I'm sorry, I'm sorry!"
+            ],
+            [
+                " "
+            ],
+            [
+                "It was an accident!",
+                "I didn't mean to!"
+            ]
+        ],
+        [
+            [
+                "Kazu: "
+            ],
+            [
+                "An accident?",
+                "It was an accident?"
+            ],
+            [
+                " "
+            ],
+            [
+                "An accident?!"
+            ],
+            [
+                " "
+            ],
+            [
+                "How was that an accident?!",
+                "How the hell was that an accident?!"
+            ]
+        ],
+        [
+            [
+                "Tako: "
+            ],
+            [
+                "I... I...",
+                "I... I'm..."
+            ]
+        ],
+        [
+            [
+                "Kazu: "
+            ],
+            [
+                "\"Oh, I... I...\"",
+                "\"Oh, I... I'm...\""
+            ],
+            [
+                " "
+            ],
+            [
+                "Stop!",
+                "Enough!",
+                "Shut up!"
+            ]
+        ],
+        [
+            [
+                "  "
+            ],
+            [
+                "Because of your failures,",
+                "Because of you,",
+                "Because of your failure,"
+            ],
+            [
+                " "
+            ],
+            [
+                "everyone will die!",
+                "everyone is going to die!",
+                "everyone is dying!",
+            ],
+            [
+                " "
+            ],
+            [
+                "Because of your failures, YOUR failures!",
+                "Because of you, of YOU!"
+            ]
+        ],
+        [
+            [
+                "Tako: "
+            ],
+            [
+                "We can fi-",
+                "We can still fi-"
+            ]
+        ],
+        [
+            [
+                "Kazu: "
+            ],
+            [
+                "This can't be fixed!",
+                "We can't fix this!",
+                "This cannot be fixed!"
+            ],
+            [
+                " "
+            ],
+            [
+                "You asshole!",
+                "You piece of shit!",
+                "You piece of crap!"
+            ]
+        ],
+        [
+            [
+                "Tako: "
+            ],
+            [
+                "Me? Asshole?",
+                "Me? An asshole?",
+                "Me? Piece of shit?",
+                "Me? Piece of crap?"
+            ],
+            [
+                " "
+            ],
+            [
+                "Right now,",
+                "Now,"
+            ],
+            [
+                " "
+            ],
+            [
+                "you're being an asshole,",
+                "you're the asshole,",
+                "you're being a piece of shit,",
+                "you're being a piece of crap,"
+            ],
+            [
+                " "
+            ],
+            [
+                "you pain in the ass!",
+                "you annoying bastard!",
+                "you're such a pain in the ass!"
+            ]
+        ],
+        [
+            [
+                "Kazu: "
+            ],
+            [
+                "Huh?!",
+                "What?!"
+            ],
+            [
+                " "
+            ],
+            [
+                "What the hell did you just say?!",
+                "What the hell did you say?!",
+                "What did you just say?!",
+                "What did you say?!"
+            ]
+        ],
+        [
+            [
+                "Tako: "
+            ],
+            [
+                "Uh...",
+                "Uhm..."
+            ],
+            [
+                " "
+            ],
+            [
+                "Nothing,"
+            ],
+            [
+                " "
+            ],
+            [
+                "goodbye!",
+                "gotta go!",
+                "bye!",
+                "goodbye forever!"
+            ]
+        ],
+        [
+            [
+                "Kazu:"
+            ],
+            [
+                "That bastard...",
+                "That son of a bitch...",
+                "That bitch..."
             ]
         ]
     ]
